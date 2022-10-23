a1e5f9492cd54522a0c822adf3632a87db1f19d1
Cormac Pujals <cmpujals@gmail.com>
2022-10-22 22:22:16 -0700

    adds create and delete functionality

--- models/skill.js
+++ models/skill.js
@@ -1,13 +1,37 @@
 const skills = [
-  {id: 123123, skill: 'JavaScript', proficiency: 'Intermediate-Mid'},
-  {id: 123124, skill: 'HTML', proficiency: 'Novice-High'},
-  {id: 123125, skill: 'CSS', proficiency: 'Novice-High'},
+  {id: 123123, skill: 'JavaScript', proficiency: 'Intermediate-Mid', done: true},
+  {id: 123124, skill: 'HTML', proficiency: 'Novice-High', done:true},
+  {id: 123125, skill: 'CSS', proficiency: 'Novice-High', done: true},
 ]
 
 module.exports = {
   getAll,
-  getOne
+  getOne,
+  create,
+  deleteOne,
+  update,
 };
+
+function update(id, updatedSkill) {
+  id = parseInt(id);
+  const skill = skills.find(skill => skill.id === id);
+  Object.assign(skill, updatedSkill);
+}
+
+function deleteOne(id) {
+  // find the index for the todo
+  id = parseInt(id);
+  const idx = skills.findIndex(skill => skill.id === id);
+  skills.splice(idx, 1);
+}
+
+function create(skill) {
+  // add the id
+  skill.id = Date.now() % 1000000;
+  // add the done property
+  skill.done = false;
+  skills.push(skill); 
+}
 
 function getOne(id) {
   id = parseInt(id);
--- routes/skills.js
+++ routes/skills.js
@@ -4,8 +4,18 @@ var skillsCtrl = require('../controllers/skills');
 
 // GET request to /todos
 router.get('/', skillsCtrl.index);
-
-// get /todos/:id
+// GET /skills/new 
+router.get('/new', skillsCtrl.new);
+// get /skills/:id
 router.get('/:id', skillsCtrl.show);
+// GET /skills/:id/edit
+router.get('/:id/edit', skillsCtrl.edit);
+// POST /skills
+router.post('/', skillsCtrl.create);
+// DELETE /skills/:id
+router.delete('/:id', skillsCtrl.delete);
+// PUT /skills/:id
+router.put('/:id', skillsCtrl.update);
+
 
 module.exports = router;
